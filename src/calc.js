674e3e07dbede36c3659256c0eb193e4be0492ad
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-04-07 18:04:46 -0700

    Class variant

--- src/calc.js
+++ src/calc.js
@@ -1,4 +1,4 @@
-export const direction = (origin, pos) => {
+export const dir = (origin, pos) => {
     let dx = pos.x - origin.x
     let dy = pos.y - origin.y
     if (dx !== 0) dx = dx / Math.abs(dx)
@@ -6,23 +6,28 @@ export const direction = (origin, pos) => {
     return { dx, dy }
 }
 
+export const rev = (origin, pos) => {
+    const { dx, dy } = dir(origin, pos)
+    return { dx: -dx, dy: -dy }
+}
+
 export const dist = ({ x: ox, y: oy }, { x: px, y: py }) => Math.sqrt((px - ox) ** 2 + (py - oy) ** 2)
 
-// adjacent
-export const adj_directions = ({ dx, dy }) => {
+// adjacent based on eight directions: adj(N) => [NE, NW]
+export const adj = ({ dx, dy }) => {
     if (dx === 0) return [{ dx: -1, dy }, { dx: 1, dy }]
     if (dy === 0) return [{ dx, dy: -1 }, { dx, dy: 1 }]
     return [{ dx, dy: 0 }, { dx: 0, dy }]
 }
 
-// tangent: use for boundaries
-export const tan_directions = ({ dx, dy }, intersect) => {
-    if (intersect.x && intersect.y) 
-        return [{ dx: -dx, dy: 0 }, { dx: 0, dy: -dy }]
-
-    if (dx === 0 || intersect.x) 
-        return [{ dx: 0, dy: -1 }, { dx: 0, dy: 1 }]
-    if (dy === 0 || intersect.y) 
-        return [{ dx: -1, dy: 0 }, { dx: 1, dy: 0 }]
+// tangent based on perpendiculars:
+// tan(N) => [E, W]
+// tan(NE) => [S, W]
+// Use for intersection with boundaries, intersection = { dx, dy }
+export const tan = ({ dx, dy }) => {
+    if (dx === 0) return [{ dx: -1, dy: 0 }, { dx: 1, dy: 0 }]
+    if (dy === 0) return [{ dx: 0, dy: -1 }, { dx: 0, dy: 1 }]
+    return [{ dx: -dx, dy: 0 }, { dx: 0, dy: -dy }]
 }
 
+export default { dir, dist, adj, tan }
--- src/stella.js
+++ src/stella.js
@@ -1,140 +1,147 @@
-import { adj_directions, direction, dist, tan_directions } from "./calc"
+import { dir, dist, adj, tan } from "./calc"
 
-class Creature {
-    constructor({ x, y, size, speed, color, last_position, proximity }) {
-        this.x = x
-        this.y = y
-        this.size = size
-        this.speed = speed
-        this.color = color
-        this.prox = proximity
-        this.last_pos = last_position
+const is_movable = (player, creature) => (
+    creature.last_position.x !== player.x
+    || creature.last_position.y !== player.y
+) && dist(player, creature) <= creature.proximity
+
+const target_ahead = (player, creature, { width, height }) => {
+    const { dx, dy } = dir(player, creature)
+    const [ sx, sy ] = [
+        creature.x + dx * creature.speed,
+        creature.y + dy * creature.speed
+    ]
+    if (sx < width && sx > 0 && sy < height && sy > 0) {
+        creature.move({ x: sx, y: sy })
+        return;
     }
 
-    move({ x, y }) {
-        this.x = x
-        this.y = y
+    const intersect = {
+        dx: sx > width ? 1 : sx < 0 ? -1 : 0,
+        dy: sy > height ? 1 : sy < 0 ? -1 : 0
     }
-
-    render(p) {
-        p.fill(...this.color)
-        p.noStroke()
-        p.circle(this.x, this.y, this.size)
-    }
+    const [ a, b ] = tan(intersect)
+    a.x = creature.x + a.dx * creature.speed
+    a.y = creature.y = a.dy * creature.speed
+    b.x = creature.x + b.dx * creature.speed,
+    b.y =  creature.y + b.dy * creature.speed
+    if (dist(player, a) > dist(player, b))
+        creature.move(a)
+    else creature.move(b)
 }
 
-/* Target position equidistant from point to ghost as ghost to player */
-class Red extends Creature {
-    constructor({ x, y }, { x: px, y: py }) {
-        super({ 
-            x, y, size: 10, speed: 5, 
-            color: [255, 0, 0], 
-            proximity: 50,
-            last_position: { x: px, y: py }
-        })
+const target_step = (player, creature, { width, height }) => {
+    const { dx, dy } = dir(player, creature)
+    const [ a, b ] = dx === 0 || dy === 0
+        ? tan({ dx, dy })
+        : adj({ dx, dy })
+    a.x = player.x + a.dx * player.speed
+    a.y = player.y + a.dy * player.speed
+    b.x = player.x + b.dx * player.speed
+    b.y = player.y + b.dy * player.speed
+
+
+
+    const da = dir(creature, a)
+    const c_a = { 
+        x: creature.x + da.dx * creature.speed,
+        y: creature.y + da.dy * creature.speed
     }
-
-    updatePosition({ x, y }, { width, height }, corral) {
-        if ((this.last_pos.x === x && this.last_pos.y === y) 
-        || dist({ x, y }, { x: this.x, y: this.y }) > this.prox) 
-            return;
-
-        this.last_pos.x = x
-        this.last_pos.y = y
-
-        const { dx, dy } = direction({ x, y }, this)
-        const [sx, sy] = [this.x + dx * this.speed, this.y + dy * this.speed]
-        if (sx < width && sx > 0 && sy < height && sy > 0) {
-            this.move({ x: sx, y: sy })
-            return;
-        }
-
-        const intersect = {
-            x: sx > width ? 1 : sx < 0 ? -1 : 0,
-            y: sy > height ? 1 : sy < 0 ? -1 : 0
-        }
-        const [a, b] = tan_directions({ dx, dy }, intersect)
-        const [ax, ay] = [this.x + a.dx * this.speed, this.y + a.dy * this.speed]
-        const [bx, by] = [this.x + b.dx * this.speed, this.y + b.dy * this.speed]
-        const dist_a = dist({ x, y }, { x: ax, y: ay })
-        const dist_b = dist({ x, y }, { x: bx, y: by })
-
-        if (dist_a > dist_b)
-            this.move({ x: ax, y: ay })
-        else
-            this.move({ x: bx, y: by })
+    const db = dir(creature, b)
+    const c_b = {
+        x: creature.x + db.dx * creature.speed,
+        y: creature.y + db.dy * creature.speed
     }
-
-    
+    if (dist(player, c_a) > dist(player, c_b))
+        creature.move(c_a)
+    else creature.move(c_b)
 }
 
-class Pink extends Creature {
-    constructor({ x, y }, { x: px, y: py }) {
-        super({
-            x, y, size: 10, speed: 5,
-            color: [188, 143, 143],
-            proximity: 50,
-            last_position: { x: px, y: py }
-        })
+const target_offset = (player, creature, { width, height }) => {}
+
+const target_corner = (player, creature, { width, height }) => {}
+
+const target_curve = (player, creature, { width, height }) => {}
+
+const red_creature = (creature) => ({
+    ...creature,
+    size: 10, 
+    speed: 5, 
+    color: [255, 0, 0], 
+    proximity: 50,
+    update_position(player, dims) {
+        if (!is_movable(player, this)) return;
+        this.last_position = { x: player.x, y: player.y }
+        target_ahead(player, this, dims)
     }
+})
 
-    updatePosition({ x, y, speed }, { width, height }, corral) {
-        if ((this.last_pos.x === x && this.last_pos.y === y) 
-        || dist({ x, y }, { x: this.x, y: this.y }) > this.prox) 
-            return;
-
-        this.last_pos.x = x
-        this.last_pos.y = y
-
-        const { dx, dy } = direction({ x, y }, this)
-        const [ left, right ] = adj_directions({ dx, dy })
-        const lx = x + left.dx * speed
-        const ly = y + left.dy * speed
-        const rx = x + right.dx * speed
-        const ry = y + right.dy * speed
-
-        const { dx: ax, dy: ay } = direction({ x: lx, y: ly }, this)
-        const { dx: bx, dy: by } = direction({ x: rx, y: ry }, this)
+const pink_creature = (creature) => ({
+    ...creature,
+    size: 10,
+    speed: 5,
+    color: [188, 143, 143],
+    proximity: 100,
+    update_position(player, dims) {
+        if (!is_movable(player, this)) return;
+        this.last_position = { x: player.x, y: player.y }
+        target_step(player, this, dims)
     }
-}
+})
 
-class Blue extends Creature {
-    constructor({ x, y }, { x: px, y: py }) {
-        super({
-            x, y, size: 10, speed: 5,
-            color: [0, 255, 0],
-            proximity: 50,
-            last_position: { x: px, y: py }
-        })
+const blue_creature = (creature) => ({
+    ...creature,
+    size: 10,
+    speed: 5,
+    color: [0, 255, 0],
+    proximity: 50,
+    update_position(player, dims) {
+        if (!is_movable(player, this)) return;
+        this.last_position = { x: player.x, y: player.y }
+        target_offset(player, this, dims)
     }
+})
 
-    updatePosition(player, dims) {}
-}
-
-class Orange extends Creature {
-    constructor({ x, y }, { x: px, y: py }) {
-        super({
-            x, y, size: 10, speed: 5,
-            color: [255, 127, 0],
-            proximity: 50,
-            last_position: { x: px, y: py }
-        })
+const orange_creature = (creature) => ({
+    ...creature,
+    size: 10,
+    speed: 5,
+    color: [255, 127, 0],
+    proximity: 50,
+    update_position(player, dims) {
+        if (!is_movable(player, this)) return;
+        const { x, y } = player
+        this.last_position = { x, y }
+        target_corner(player, this, dims)
     }
+})
 
-    updatePosition(player, dims) {}
-}
 
 const types = {
-    red: Red,
-    pink: Pink,
-    blue: Blue,
-    orange: Orange
+    red: red_creature,
+    pink: pink_creature,
+    blue: blue_creature,
+    orange: orange_creature
 }
 
-export const createCreature = ({ width, height }, player, type, position = { 
-    x: Math.random() * width, 
-    y: Math.random() * height 
+export const createCreature = (dims, player, type, position = { 
+    x: Math.random() * dims.width, 
+    y: Math.random() * dims.height 
 }) => {
     if (!types.hasOwnProperty(type)) return false;
-    return new types[type](position, player)
+    const { x, y } = player
+    const creature = {
+        ...position,
+        last_position: { x, y },
+        move({ x, y }) {
+            this.x = x
+            this.y = y
+        },
+        render(p) {
+            p.fill(...this.color)
+            p.noStroke()
+            p.circle(this.x, this.y, this.size)
+        }
+    }
+    return types[type](creature)
 }
